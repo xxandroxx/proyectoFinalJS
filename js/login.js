2d54ef9f064884a12e61020a9e9b47a56bcf08a4
Sandro Ovando <sandro.ovando@gmail.com>
2022-01-05 16:56:55 -0300

    login terminado

--- js/login.js
+++ js/login.js
@@ -8,11 +8,13 @@ const popUp = document.querySelector('.contenedor-popup');
 const btnCerrarPopUp = document.querySelector('#btn-cerrar-ayuda');
 
 //Formulario
-const inputUsuario = document.querySelector('#input-usuario').value;
-const inputContraseña = document.querySelector('#input-contraseña').value;
+const inputUsuario = document.querySelector('#input-usuario');
+const inputContraseña = document.querySelector('#input-contraseña');
 const btnLogin = document.querySelector('#btn-login');
 const incorrectoUsuario = document.querySelector('#incorrecto-usuario');
-
+const incorrectoContraseña = document.querySelector('#incorrecto-contraseña');
+const formularioLogin = document.querySelector('#formulario-login');
+const contenedorLogin = document.querySelector('.contenedor-login');
 
 
 
@@ -20,16 +22,41 @@ const incorrectoUsuario = document.querySelector('#incorrecto-usuario');
 //Formulario
 
 //Funciones
+formularioLogin.addEventListener('submit', e => e.preventDefault());
 
 //Validar formulario
-const validarFormulario = (e) =>{
-    if(inputUsuario != 'admin'){
+const validarFormulario = () =>{
+    
+    if(inputUsuario.value.length < 1){
+        incorrectoUsuario.innerHTML = 'Debe ingresar un usuario';
+    }else if(inputUsuario.value != 'admin'){
         incorrectoUsuario.innerHTML = 'Usuario incorrecto';
-    }else if(inputUsuario.lenght < 1){
-        incorrectoUsuario.innerHTML = 'Debe ingresar usuario';
+    }else{
+        incorrectoUsuario.innerHTML = '';
     }
 
-    e.preventDefault();
+    if(inputContraseña.value.length <1){
+        incorrectoContraseña.innerHTML = 'Debe ingresar una contraseña'
+    }else if(inputContraseña.value != 'admin'){
+        incorrectoContraseña.innerHTML = 'Contraseña incorrecta';
+    }else{
+        incorrectoContraseña.innerHTML = '';
+    }
+
+
+    if(inputUsuario.value === 'admin' && inputContraseña.value === 'admin'){
+
+        incorrectoUsuario.innerHTML = 'Correcto';
+        incorrectoUsuario.style.color = 'green';
+        incorrectoContraseña.innerHTML = 'Correcto';
+        incorrectoContraseña.style.color = 'green';
+
+        setTimeout(() => {
+           contenedorLogin.style.display = 'none' 
+        }, 1000);
+    }
+
+
 }
 
 
@@ -48,6 +75,7 @@ btnCerrarPopUp.addEventListener('click', () =>{
 });
 
 
+//Formulario
 btnLogin.addEventListener('click', validarFormulario);
 
 
